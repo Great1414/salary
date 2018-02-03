ab6a8a9c94bb440cb02a69f3a7e6bb2e5a23f2bf
r1550562056 <r1550562056@163.com>
2018-02-03 15:13:07 +0800

    "challenge3"

--- calculator.py
+++ calculator.py
@@ -1,21 +1,75 @@
 #!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 import sys
+import csv
 
+#argv
+class Args(object):
+
+    def __init__(self):
+        self.args = self.sys.argv[1:]
+
+#configfile
+
+class Config(Args):
+
+    def __init__(self):
+        self.config = self._read_config()
+
+    def _read_config(self):
+        config = {}
+        with open(sys.argv[1]) as confignews:
+            for one_config in confignews:
+                news = one_config.strip().split('=')[0]
+                nums = one_config.strip().split('=')[1]
+                config = dict(zip(news,nums))
+        return config
+
+#user data
+class UserData(Args):
+    
+    def __init__(self):
+        self.userdata = self._read_users_data
+    
+    def _read_users_data(self):     
+        userdata = {}
+        with open(syis.argv[2]) as datanews:
+            for one_data in datanews:
+                idnum = one_data.strip().split('=')[0]
+                wage = one_data.strip().split('=')[1]
+                userdata = (idnum.wage)
+        return userdata
+
+
+#tax_income
+class IncomeTax():
+    #salary after tax
+    def calc_fordata(self):
+    #social security tax
+    for oneid in userdata._read_user_data:
+        if oneid[1] < config._read_config('JiShuL'):
+            sstax = config._read_config('JiShuL')*0.165
+        elif  oneid[1] > config._read_config('JiShuH'):
+            sstax = config._read_config('JiShuH')*0.165
+        else:
+            sstax = oneid[1]*0.165
+
+    #tax for oneself
 def tax(taxwage):
 
     if taxwage<=0:
         return 0
-    elif 0<taxwage<=1500:
+    elif taxwage<=1500:
         return taxwage*0.03
-    elif 1500<taxwage<=4500:
+    elif taxwage<=4500:
         return taxwage*0.1-105
-    elif 4500<taxwage<=9000:
+    elif taxwage<=9000:
         return taxwage*0.2-555
-    elif 9000<taxwage<=35000:
+    elif taxwage<=35000:
         return taxwage*0.25-1005
-    elif 35000<taxwage<=55000:
+    elif taxwage<=55000:
         return taxwage*0.3-2755
-    elif 55000<taxwage<=80000:
+    elif taxwage<=80000:
         return taxwage*0.35-5505
     else:
         return taxwage*0.45-13505
@@ -33,4 +87,4 @@ if __name__=='__main__':
         #for key, value in salary.items():
         print('{}:{:.2f}'.format(num,money))
 
-
+#exec
