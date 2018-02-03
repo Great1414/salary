b7ca58d618a1fd0bfb54acf6b9e484fd3e4822d9
r1550562056 <r1550562056@163.com>
2018-02-03 16:11:53 +0800

    "new version challenge3"

--- calculator.py
+++ calculator.py
@@ -43,48 +43,55 @@ class UserData(Args):
 
 #tax_income
 class IncomeTax():
+    comployeedata = []
     #salary after tax
     def calc_fordata(self):
     #social security tax
-    for oneid in userdata._read_user_data:
-        if oneid[1] < config._read_config('JiShuL'):
-            sstax = config._read_config('JiShuL')*0.165
-        elif  oneid[1] > config._read_config('JiShuH'):
-            sstax = config._read_config('JiShuH')*0.165
-        else:
-            sstax = oneid[1]*0.165
+        for oneid in userdata._read_user_data:
+            if oneid[1] < config._read_config('JiShuL'):
+                sstax = config._read_config('JiShuL')*0.165
+            elif  oneid[1] > config._read_config('JiShuH'):
+                sstax = config._read_config('JiShuH')*0.165
+            else:
+                sstax = oneid[1]*0.165           
+            taxwage = oneid[1] - sstax
+              
 
     #tax for oneself
-def tax(taxwage):
+   # def tax(taxwage):
 
-    if taxwage<=0:
-        return 0
-    elif taxwage<=1500:
-        return taxwage*0.03
-    elif taxwage<=4500:
-        return taxwage*0.1-105
-    elif taxwage<=9000:
-        return taxwage*0.2-555
-    elif taxwage<=35000:
-        return taxwage*0.25-1005
-    elif taxwage<=55000:
-        return taxwage*0.3-2755
-    elif taxwage<=80000:
-        return taxwage*0.35-5505
-    else:
-        return taxwage*0.45-13505
+     	if taxwage<=0:
+	    tax_self = 0
+        elif taxwage<=1500:
+	    tax_self = taxwage*0.03
+	elif taxwage<=4500:
+	    tax_sel f= taxwage*0.1-105
+	elif taxwage<=9000:
+	    tax_self = taxwage*0.2-555
+	elif taxwage<=35000:
+	    tax_self =  taxwage*0.25-1005
+	elif taxwage<=55000:
+	    tax_self = taxwage*0.3-2755
+	elif taxwage<=80000:
+	    tax_self = taxwage*0.35-5505
+	else:
+	    tax_self = taxwage*0.45-13505
+	get_money = oneid[1] - sstax - tax_self
+       	comployeedata = [oneid[0], oneid[1], sstax, tax_self, get_money ]
+       return comployeedata
 
-if __name__=='__main__':    
+    #output csv file
+    def export(self, default = 'csv')
+        result = self.calc_fordata()
+        with open(sys.argv[3]) as f:
+            writer = csv.writer(f)
+            writer.writerows(result)
+
+#exec
+
+if __name__=='__main__':   
+ 
     for arg in sys.argv[1:]:
-        try:    
-            num = int(arg.split(':')[0])
-            wage = int(arg.split(':')[1])
-        except:
-            print('Parameter Error')
-        taxwage =  wage*(1-0.165)-3500
-        money = wage*(1-0.165) - tax(taxwage)
-        #salary = dict(num = money)
-        #for key, value in salary.items():
-        print('{}:{:.2f}'.format(num,money))
+        print('{0},{1},{2:.2f},{3:.2f},{4:.2f}'.format(self.calc_fordata()))
 
 #exec
