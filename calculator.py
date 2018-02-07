458f712b3e6119cc44245b85b5bb77a2ed043031
r1550562056 <r1550562056@163.com>
2018-02-07 22:51:55 +0800

    "challenge3-best"

--- calculator.py
+++ calculator.py
@@ -32,14 +32,16 @@ class Config(object):
         self.config = self._read_config()
 
     def _read_config(self):
-        config_path = args.config_path()
+        config_path = args.config_path
         config = {}  
         with open(config_path) as confignews:
             for one_config in confignews.readlines():
-                news = one_config.strip().split('=')[0]
-                nums = one_config.strip().split('=')[1]
-                config = dict(zip(news,nums))
+               # news = one_config.strip().split('=')[0]
+               news, nums = one_config.strip().split('=')
+               config[news.strip()] = float(nums.strip())
         return config
+    def get_config(self, key):
+        return self.config[key]
 
 config = Config()
 
@@ -50,7 +52,7 @@ class UserData(object):
         self.userdata = self._read_users_data()
     
     def _read_users_data(self):
-        userdata_path = args.userdata_path()     
+        userdata_path = args.userdata_path     
         userdata = []
         with open(userdata_path) as datanews:
             for one_data in datanews.readlines():
@@ -58,11 +60,11 @@ class UserData(object):
                # wage = one_data.strip().split(',')[1]
                # userdata = (idnum.wage)
                 idnum, wage = one_data.strip().split(',')
-                userdata.append((idnum, wage))
+                userdata.append((idnum, int(wage)))
         return userdata
 
-def __iter__(self):
-    return iter(self.userdata)
+    def __iter__(self):
+        return iter(self.userdata)
 
 
 #tax_income
@@ -74,10 +76,10 @@ class IncomeTax(object):
     @staticmethod
     def calc_fordata(wage):
     #social security tax
-        if wage < config._read_config('JiShuL'):
-            return config._read_config('JiShuL')*0.165
-        if wage > config._read_config('JiShuH'):
-            return config._read_config('JiShuH')*0.165
+        if wage < config.get_config('JiShuL'):
+            return config.get_config('JiShuL')*0.165
+        if wage > config.get_config('JiShuH'):
+            return config.get_config('JiShuH')*0.165
         else:
             return wage*0.165
        # for oneid in userdata._read_user_data:
@@ -92,7 +94,7 @@ class IncomeTax(object):
 # tax_oneself
     @classmethod
     def tax_for_one(cls, wage):
-        social_wage = cls.cal_fordata(wage) 
+        social_wage = cls.calc_fordata(wage) 
         taxwage = wage - social_wage       
         if taxwage<=0:
             tax_self = 0
@@ -115,9 +117,15 @@ class IncomeTax(object):
     def money(self):
         comployeedata= []
         for idnum, wage in self.userdata:
-            get_money = wage -self.calc_fordata() - self.tax_for_one
-            comployeedata.append = ('{}'.idnum,'{}'.wage,'{:.2f}'.self.calc_fordata(), '{:.2f}'.self.tax_for_one,'{:.2f}'.get_money)
+            comployee = [idnum, wage]
+            shebao = self.calc_fordata(wage)
+            taxone = self.tax_for_one(wage)
+            get_money = wage - shebao - taxone
+            comployee  += ['{:.2f}'.format(shebao), '{:.2f}'.format(taxone),'{:.2f}'.format(get_money)]
+            comployeedata.append(comployee)
         return comployeedata
+    def __iter__(self):
+        return iter(self.userdata)
 
     #output csv file
     def export(self, default = 'csv'):
